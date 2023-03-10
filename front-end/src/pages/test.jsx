a30a6a8c50da1b0a653c55fd8587d9dc007c3ed9
rylynntruth <yoy159@naver.com>
2023-03-10 11:52:10 +0900

    2차 배포
    2차 배포

--- front-end/src/pages/test.jsx
+++ front-end/src/pages/test.jsx
@@ -2,3 +2,9 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
+
+const Test = () => {
+
+}
+
+export default Test;
